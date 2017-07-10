5996577b2dfb012402df1dcc098f294e6f5ed6b9
Henk-Jan <neastre@gmail.com>
2017-07-10 22:38:50 +0200

    First setup of Main Tower
    + Shooting

---
+++ Programming/UnityProject/TowerDefence/Assets/Scripts/TowerBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBase : MonoBehaviour {
+
+    [SerializeField]
+    protected float attackSpeed = 1, attackRange = 1, maxHealth, health = 100, armor = 0;
+
+	void Start ()
+    {
+        maxHealth = health;
+	}
+	
+
+	void Update ()
+    {
+		
+	}
+
+    protected void UpgradeAttackSpeed()
+    {
+        attackSpeed *= 1.01f;
+    }
+
+    protected void UpgradeAttackRange()
+    {
+        attackRange *= 1.01f;
+    }
+
+    protected void UpgradeMaxHealth()
+    {
+        maxHealth += 10;
+    }
+}
---
+++ Programming/UnityProject/TowerDefence/Assets/Scripts/TowerMain.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerMain : MonoBehaviour
+{
+    public GameObject gunPoint;
+
+    void Start ()
+    {
+        //attackRange = 10;
+    }
+
+	void Update ()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        RaycastHit hit;
+        Vector3 dir = other.transform.position - gunPoint.transform.position;
+        if (Physics.Raycast(gunPoint.transform.position, dir, out hit))
+        {
+            DrawLine(gunPoint.transform.position, hit.point, Color.black);
+            //Destroy(other.gameObject);
+        }
+    }
+    
+    void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.2f)
+    {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+        //lr.StartColor(Color.black);
+        lr.SetColors(color, color);
+        lr.SetWidth(0.1f, 0.1f);
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        GameObject.Destroy(myLine, duration);
+    }  
+}
