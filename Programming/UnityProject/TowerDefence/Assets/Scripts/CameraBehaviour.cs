d5184e515c5708c531320fbdffe63af7188d0131
Henk-Jan <neastre@gmail.com>
2017-07-13 17:14:02 +0200

    Code Cleanup

--- Programming/UnityProject/TowerDefence/Assets/Scripts/CameraBehaviour.cs
+++ Programming/UnityProject/TowerDefence/Assets/Scripts/CameraBehaviour.cs
@@ -8,19 +8,18 @@ public class CameraBehaviour : MonoBehaviour {
     private float runSpeed = 10f;
     private float zoomSpeed = 25f;
     private float rotationSpeed = 5f;
-    private float LookMaxAngle = 45.0f; //max viewing angle up/down
-    private float lookRotationX = 0.0f;          //holds current left/right rotation of camera
-    private float lookRotationY = 0.0f;          //holds current up/down rotation of camera
+    private float LookMaxAngle = 45.0f;             //max viewing angle up/down
+    private float lookRotationX = 0.0f;             //holds current left/right rotation of camera // Rotade around Y-axis
+    private float lookRotationY = 30.0f;            //holds current up/down rotation of camera    // Rotade around X-axis
 
     float cameraHeight = 10f;
-
-    public Camera camera;
 
     Rigidbody rigidbody;
 
     void Start ()
     {
         rigidbody = GetComponent<Rigidbody>();
+        transform.localRotation = Quaternion.Euler(lookRotationY, lookRotationX, 0);
     }
 	
 	void Update ()
@@ -52,14 +51,17 @@ public class CameraBehaviour : MonoBehaviour {
         {
             velocity.z += speed * Time.deltaTime * 100;
         }
+
         if (Input.GetKey(KeyCode.S))
         {
             velocity.z -= speed * Time.deltaTime * 100;
         }
+
         if (Input.GetKey(KeyCode.A))
         {
             velocity.x -= speed * Time.deltaTime * 100;
         }
+
         if (Input.GetKey(KeyCode.D))
         {
             velocity.x += speed * Time.deltaTime * 100;
@@ -73,38 +75,12 @@ public class CameraBehaviour : MonoBehaviour {
         {
             cameraHeight += zoomSpeed * Time.deltaTime;
         }
-        
-
-        //rigidbody.velocity = transform.TransformVector(velocity); //overwrite existing velocity with our own
         rigidbody.velocity = Vector3.Lerp(transform.TransformVector(velocity), Vector3.zero, 0.2f); //overwrite existing velocity with our own
+        
+        // lock Y-axis to cameraHeight
         temp.x = transform.position.x;
         temp.y = cameraHeight;
         temp.z = transform.position.z;
         transform.position = temp;
-        
-        /*
-        Vector3 velocity = transform.position;
-        float temp = velocity.y;
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            velocity.z += speed * Time.deltaTime;
-            //velocity += camera.transform.forward * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            velocity.z -= speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            velocity.x -= speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            velocity.x += speed * Time.deltaTime;
-        }
-        velocity.y = temp;
-        transform.position = velocity;
-        */
     }
 }
